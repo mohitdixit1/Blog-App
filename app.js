d2ee99f301ea6e9ab0263a4fcf47406bc360cb3b
mohitdixit1 <dixitmohitdixit4@gmail.com>
2025-06-19 19:34:09 +0530

    improve ui

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-require('dotenv').config();
 
+require('dns').setDefaultResultOrder('ipv4first');
+require('dotenv').config();
+const express = require("express");
+const app = express();
 const path = require("path")
+app.use(express.static(path.resolve("./public")));
+
+
 const UserRoute = require("./routes/user");
 const BlogRoute = require("./routes/blog")
 const Blog = require("./models/blog")
 
 const cookieParser = require("cookie-parser")
 const {cheackForAuthCookie} = require("./middlewares/authentication")
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const express = require("express");
-const app = express();
+
 
 const PORT = process.env.PORT ||7000;
-mongoose.connect(process.env.MONGO_URL).then((e)=>console.log("mongoose is connected")).catch((error)=>{
-    console.log(`error to connect mongoose -: ${error}`)
-})
+
+
+
+
+
+mongoose.connect( process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.error("Connection error:", err));
+
 
 app.set("view engine" , "ejs")
 app.set("views",path.resolve("./views"))
@@ -28,7 +39,8 @@ app.use("/blog",BlogRoute);
 app.use(express.static(path.resolve("./public")))
 
 app.get("/",async (req,res)=>{
-    const AllBlog = await Blog.find({});
+    const AllBlog = await Blog.find().sort({ createdAt: -1 });  // Newest first
+;
     if(!req.cookies.token){return res.redirect("/user/signin")
     }
     res.render("home",{
