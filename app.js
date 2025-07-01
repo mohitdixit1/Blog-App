8b80fd022502aa2ef33f6cb661e508ab1a3f008a
mohitdixit1 <dixitmohitdixit4@gmail.com>
2025-07-01 19:51:03 +0530

    .

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("Connection error:", err));
 
